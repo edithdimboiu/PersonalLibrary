e32a5f58d6b077586d74962e62c0ac3316e566c6
Edith Dimboiu <edith@dimboiu.com>
2024-03-01 13:56:15 +0100

    update useGet hook to get maximum 40 results instead of 12

--- src/hooks/useGet.jsx
+++ src/hooks/useGet.jsx
@@ -9,7 +9,7 @@ export const useGet = query => {
     if (query && query.length > 3) {
       setIsLoading(true);
       const key = "AIzaSyCrN9f1ugzEZns_Gp8O_8E7po1iBXsQvGY";
-      const url = `https://www.googleapis.com/books/v1/volumes?q=${query}&maxResults=12&key=${key}`;
+      const url = `https://www.googleapis.com/books/v1/volumes?q=${query}&maxResults=40&key=${key}`;
 
       fetch(url)
         .then(response => response.json())
