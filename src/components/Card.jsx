cf859c2db24c1be17fe05f4aef9b7f41bf316d05
Edith Dimboiu <edith@dimboiu.com>
2024-03-01 13:25:13 +0100

    create Card component

---
+++ src/components/Card.jsx
@@ -0,0 +1,24 @@
+import { CiCalendar } from "react-icons/ci";
+import noImage from "../assets/noImage.jpeg";
+
+export default function Card({ book }) {
+  return (
+    <li>
+      <img
+        src={book.volumeInfo.imageLinks?.smallThumbnail || noImage}
+        alt={`${book.volumeInfo?.title} image`}
+      />
+      <h3>{book.volumeInfo?.title}</h3>
+      <h4>{book.volumeInfo?.authors}</h4>
+      <div>
+        <p>
+          <CiCalendar />
+          <span>
+            {new Date(book.volumeInfo?.publishedDate).getFullYear() ||
+              "Unknown release year"}
+          </span>
+        </p>
+      </div>
+    </li>
+  );
+}
