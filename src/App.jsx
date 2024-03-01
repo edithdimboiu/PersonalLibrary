e58039d8010639ba33459164d4a7bb4a6dc487b7
Edith Dimboiu <edith@dimboiu.com>
2024-03-01 13:48:23 +0100

    fetch the data and render Section Component with children

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import "./App.css";
+import Main from "./components/Main";
 import Navigation from "./components/Navigation";
 import SearchInput from "./components/SearchInput";
-import Main from "./components/Main";
 import { useState } from "react";
+import BookList from "./components/BookList";
+import Loader from "./components/Loader";
+import { useGet } from "./hooks/useGet";
+import Section from "./components/Section";
 
 export default function App() {
   const [query, setQuery] = useState("");
+  const { books, error, isLoading } = useGet(query);
 
   function handleOnChange(e) {
     setQuery(e.target.value);
@@ -16,7 +21,13 @@ export default function App() {
       <Navigation>
         <SearchInput query={query} handleOnChange={handleOnChange} />
       </Navigation>
-      <Main />
+      <Main>
+        <Section>
+          {isLoading && <Loader />}
+          {!isLoading && !error && <BookList books={books} />}
+          {error && <h2>There was an error!</h2>}
+        </Section>
+      </Main>
     </>
   );
 }
