e9c94983e38263cdde314e70bfb696c355fba56f
Edith Dimboiu <edith@dimboiu.com>
2024-03-07 15:15:05 +0100

    use statistics component

--- src/components/MyLibrary.jsx
+++ src/components/MyLibrary.jsx
@@ -1,8 +1,10 @@
 import MyLibraryCard from "./MyLibraryCard";
+import Statistics from "./Statistics";
 import { useState } from "react";
 
 function MyLibrary({ books, handleRemoveBook }) {
   const [readBooks, setReadBooks] = useState([]);
+  const [showStatistics, setShowStatistics] = useState(false);
 
   const markAsRead = id => {
     if (readBooks.some(book => book.id === id)) {
@@ -23,6 +25,13 @@ function MyLibrary({ books, handleRemoveBook }) {
             You have {books.length} books in your library, of which you read{" "}
             {readBooks.length} .
           </h2>
+          <button
+            className="btn text-btn"
+            onClick={() => setShowStatistics(!showStatistics)}
+          >
+            Statistics
+          </button>
+          {showStatistics && <Statistics readBooks={readBooks} myLibrary={books} />}
         </div>
       )}
       <ul className="list list-books">
