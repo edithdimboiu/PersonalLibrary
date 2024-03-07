8b1ab7f9b23a8d453f379d9f9bb7f7b37c18a298
Edith Dimboiu <edith@dimboiu.com>
2024-03-07 15:13:50 +0100

    create statistics component

---
+++ src/components/Statistics.jsx
@@ -0,0 +1,45 @@
+export default function Statistics({ readBooks, myLibrary }) {
+  const countedReadBooks = {};
+  const countedMyLibraryBooks = {};
+  let booksWithPrice = 0;
+
+  const readPages = readBooks.reduce((accumulator, book) => {
+    if (!countedReadBooks[book.id]) {
+      accumulator += book.volumeInfo.pageCount;
+      countedReadBooks[book.id] = true;
+    }
+    return accumulator;
+  }, 0);
+
+  const investment = myLibrary.reduce((accumulator, book) => {
+    const price = book.saleInfo?.listPrice?.amount;
+
+    if (price) {
+      accumulator += price;
+      countedMyLibraryBooks[book.id] = true;
+      booksWithPrice++;
+    }
+    return accumulator;
+  }, 0);
+
+  const booksWithoutPrice = myLibrary.length - booksWithPrice;
+  return (
+    <div className="statistics">
+      <h2>Your readings in numbers</h2>
+      <li>
+        Until now you read <strong>{readPages} pages</strong>
+      </li>
+      <li>
+        You invested aproximately <strong>{investment} CHF</strong> in your
+        personal library. We can not give you the exact amount because{" "}
+        <strong>
+          {booksWithoutPrice === 1
+            ? "there is 1 book"
+            : `there are ${booksWithoutPrice} books`}{" "}
+          without price
+        </strong>{" "}
+        in our data.
+      </li>
+    </div>
+  );
+}
